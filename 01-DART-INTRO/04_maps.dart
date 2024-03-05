c86b8950ea2b21bcd04df9267dee232b7e19a481
David Merino <merinovecinod@gmail.com>
2024-03-05 14:38:21 +0100

    iterables session (step 14)

--- 01-DART-INTRO/04_maps.dart
+++ 01-DART-INTRO/04_maps.dart
@@ -6,11 +6,11 @@ void main() {
   final List<String> sprites = ["ditto/front.png", "ditto/back.png"];
 
   // dynamic is used to note that a var has no specific type, hence it could be of any type. It is allways nullable
-  dynamic errorMessage = "generic Error";
+  /*dynamic errorMessage = "generic Error";
   errorMessage = true;
   errorMessage = [1, 2, 3, 4];
   errorMessage = {1, 2, 3, 4};
-  errorMessage = () => bool;
+  errorMessage = () => bool;*/
 
   final pokemonMap = {
     'name': pokemon,
