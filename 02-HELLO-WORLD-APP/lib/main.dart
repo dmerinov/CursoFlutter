a975347ce65170b2923410e29eb8cd653fd77d9b
David Merino <merinovecinod@gmail.com>
2024-03-06 11:32:33 +0100

    centered text

--- 02-HELLO-WORLD-APP/lib/main.dart
+++ 02-HELLO-WORLD-APP/lib/main.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   //main function that takes a widget to run.
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Center(child: Text("Hello World")));
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello World'),
+          ),
+        ));
   }
 }
