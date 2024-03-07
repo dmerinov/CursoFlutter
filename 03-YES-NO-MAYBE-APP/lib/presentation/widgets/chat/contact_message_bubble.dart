7945ce32b54a04e1386e09046a75792943dbdd3d
David Merino <merinovecinod@gmail.com>
2024-03-07 11:21:22 +0100

    04: added message when contact is sending message

--- 03-YES-NO-MAYBE-APP/lib/presentation/widgets/chat/contact_message_bubble.dart
+++ 03-YES-NO-MAYBE-APP/lib/presentation/widgets/chat/contact_message_bubble.dart
@@ -42,6 +42,15 @@ class _ImageBubble extends StatelessWidget {
         width: size.width * 0.7,
         height: 150,
         fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child; //el child sería la imagen
+          return Container(
+            width: size.width * 0.7,
+            height: 150,
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: const Text('Contacto está escribiendo...'),
+          );
+        },
       ),
     );
   }
