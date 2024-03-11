5c76eb7edb3448c770283af19a1d269e6636407d
David Merino <merinovecinod@gmail.com>
2024-03-11 10:21:32 +0100

    added provider

--- 04-TOK-TIK/lib/main.dart
+++ 04-TOK-TIK/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tok_tik/config/theme/app_theme.dart';
+import 'package:tok_tik/presentation/providers/discover_provider.dart';
+import 'package:tok_tik/presentation/screens/discover/discover_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,16 +11,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TokTik',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme().getTheme(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('TokTik'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => DiscoverProvider(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'TokTik',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme().getTheme(),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('TokTik'),
+          ),
+          body: const Center(
+            child: DiscoverScreen(),
+          ),
         ),
       ),
     );
