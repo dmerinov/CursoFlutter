4a77219905ad3a5fc723ca28e393a4e8c1143743
David Merino <merinovecinod@gmail.com>
2024-03-11 12:15:35 +0100

    player implemented

--- 04-TOK-TIK/lib/presentation/widgets/shared/video_player/video_buttons.dart
+++ 04-TOK-TIK/lib/presentation/widgets/shared/video_player/video_buttons.dart
@@ -14,30 +14,30 @@ class VideoButtons extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        _customIconButton(
+        _CustomIconButton(
             value: video.likes,
             iconData: Icons.favorite,
             iconColor: Colors.red),
         const SizedBox(height: 20),
-        _customIconButton(
+        _CustomIconButton(
             value: video.views, iconData: Icons.remove_red_eye_outlined),
         const SizedBox(height: 20),
         SpinPerfect(
             infinite: true,
             duration: const Duration(seconds: 5),
-            child: const _customIconButton(
+            child: const _CustomIconButton(
                 value: 0, iconData: Icons.play_circle_outlined))
       ],
     );
   }
 }
 
-class _customIconButton extends StatelessWidget {
+class _CustomIconButton extends StatelessWidget {
   final int value;
   final IconData iconData;
   final Color? color;
 
-  const _customIconButton(
+  const _CustomIconButton(
       {required this.value, required this.iconData, iconColor})
       : color = iconColor ?? Colors.white;
 
--- 04-TOK-TIK/lib/presentation/widgets/video/fullscreen_player.dart
+++ 04-TOK-TIK/lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class FullScreenPlayer extends StatelessWidget {
+class FullScreenPlayer extends StatefulWidget {
   final String videoUrl;
   final String caption;
 
@@ -8,7 +9,37 @@ class FullScreenPlayer extends StatelessWidget {
       {super.key, required this.videoUrl, required this.caption});
 
   @override
+  State<FullScreenPlayer> createState() => _FullScreenPlayerState();
+}
+
+class _FullScreenPlayerState extends State<FullScreenPlayer> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(widget.videoUrl)
+      ..setVolume(0)
+      ..setLooping(true)
+      ..play();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return FutureBuilder(
+      future: _controller.initialize(),
+      builder: (context, snapshot) {
+        return const Center(
+            child: CircularProgressIndicator(
+          strokeWidth: 2,
+        ));
+      },
+    );
   }
 }
