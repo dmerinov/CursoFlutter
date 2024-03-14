c6be788114f960863388497cb0bd5952dbfe8251
David Merino <merinovecinod@gmail.com>
2024-03-14 10:30:52 +0100

    added buttons to button screen

--- 05-WIDGETS-APP/lib/lib/presentation/screens/buttons/buttons_screen.dart
+++ 05-WIDGETS-APP/lib/lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
   const ButtonsScreen({super.key});
@@ -8,9 +9,85 @@ class ButtonsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ButtonsScreen'),
+        title: const Text('ButtonsScreen'),
       ),
-      body: Placeholder(),
+      body: const _ButtonsView(),
+      floatingActionButton: const _FloatingActionButton(),
+    );
+  }
+}
+
+class _FloatingActionButton extends StatelessWidget {
+  const _FloatingActionButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+        child: const Icon(Icons.arrow_back_ios_new_rounded),
+        onPressed: () {
+          context.pop();
+        });
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {}, child: const Text('elevated button')),
+            const ElevatedButton(
+                onPressed: null, child: Text('disabled button')),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.alarm),
+              label: const Text('Alarm'),
+            ),
+            FilledButton(onPressed: () {}, child: const Text('field')),
+            FilledButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.accessibility_new_rounded),
+              label: const Text('Icon'),
+            ),
+            const CustomButton()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Text(
+              'data',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          onTap: () {},
+        ),
+      ),
     );
   }
 }
