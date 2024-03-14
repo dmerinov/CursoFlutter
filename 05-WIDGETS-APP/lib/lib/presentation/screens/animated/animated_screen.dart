6c690290b38e8867ba73e83706645148fae1b1ba
David Merino <merinovecinod@gmail.com>
2024-03-14 12:06:57 +0100

    finished first part of widgets app

---
+++ 05-WIDGETS-APP/lib/lib/presentation/screens/animated/animated_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AnimatedScreen extends StatelessWidget {
+  const AnimatedScreen({super.key});
+  static const name = 'animated screen';
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
